a21181e317b6a64a50c29b1fa810d4d9e340cdea
AjayKumar <ajaykumar799719@gmail.com>
2025-06-27 02:49:09 +0530

    Added Spring Files

---
+++ src/test/java/com/strive2launch/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strive2launch.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
